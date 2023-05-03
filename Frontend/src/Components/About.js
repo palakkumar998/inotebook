727ee86404db3e0de311958bde88f4cf1598cf7f
Palak <palakkumar1998@gmail.com>
2023-05-03 22:07:51 +0530

    after setting up react components

---
+++ Frontend/src/Components/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <>
+      <div>about Components</div>
+        </>
+    )
+}
+export default About;
---
+++ Frontend/src/Components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Home = () => {
+    return (
+       <>
+      
+       <div>home comp</div>
+       </>
+    )
+}
+export default Home;
